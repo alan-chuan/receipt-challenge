6a941d78139c462d29aed787b67d98bc7b2eaf3b
Alan Chuan <chuan2@wisc.edu>
2023-09-13 21:57:11 -0400

    add new receipt contract and rspec test

---
+++ contracts/new_receipt_contract.rb
@@ -0,0 +1,20 @@
+require 'dry-validation'
+
+class NewReceiptContract < Dry::Validation::Contract
+  RETAILER_REGEX = /^\S+$/.freeze
+  TIME_REGEX = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/.freeze
+  SHORT_DESCRIPTION_REGEX = /^[\w\s\-]+$/.freeze
+  PRICE_REGEX = /^\d+\.\d{2}$/.freeze
+
+  schema do
+    required(:retailer).filled(:string){ format?(RETAILER_REGEX) }
+    required(:purchaseDate).filled(:string)
+    required(:purchaseTime).filled(:string) { format?(TIME_REGEX) }
+    required(:total).filled(:string) { format?(PRICE_REGEX) }
+
+    required(:items).array(:hash) do
+      required(:shortDescription).filled(:string) { format?(SHORT_DESCRIPTION_REGEX) }
+      required(:price).filled(:string) { format?(PRICE_REGEX) }
+    end
+  end
+end
---
+++ tests/new_receipt_contract_spec.rb
@@ -0,0 +1,92 @@
+require 'dry-validation'
+require 'rspec'
+require_relative '../contracts/new_receipt_contract'
+
+describe NewReceiptContract do
+  subject(:contract) { described_class.new }
+
+  it 'successfully validates a valid receipt' do
+    valid_data = {
+      retailer: 'Target',
+      purchaseDate: '2022-01-02',
+      purchaseTime: '13:13',
+      total: '1.25',
+      items: [
+        { shortDescription: 'Pepsi - 12-oz', price: '1.25' }
+      ]
+    }
+
+    expect(contract.call(valid_data)).to be_success
+  end
+
+  it 'rejects an invalid retailer' do
+    invalid_data = {
+      retailer: 'Invalid Retailer',
+      purchaseDate: '2022-01-02',
+      purchaseTime: '13:13',
+      total: '10000.00',
+      items: [
+        { shortDescription: 'Pepsi - 12-oz', price: '1.25' }
+      ]
+    }
+
+    expect(contract.call(invalid_data)).to_not be_success
+  end
+
+  it 'rejects an invalid purchase time' do
+    invalid_data = {
+      retailer: 'Walgreens',
+      purchaseDate: '2022-01-02',
+      purchaseTime: '4:19',
+      total: '100.21',
+      items: [
+        { shortDescription: 'Pepsi - 12-oz', price: '1.25' }
+      ]
+    }
+
+    expect(contract.call(invalid_data)).to_not be_success
+  end
+
+  it 'rejects an invalid short description' do
+    invalid_data = {
+      retailer: 'Walgreens',
+      purchaseDate: '2022-01-02',
+      purchaseTime: '13:13',
+      total: '123.40',
+      items: [
+        { shortDescription: '', price: '1.25' }
+      ]
+    }
+
+    expect(contract.call(invalid_data)).to_not be_success
+  end
+
+  it 'rejects an invalid total price' do
+    invalid_data = {
+      retailer: 'Walgreens',
+      purchaseDate: '2022-01-02',
+      purchaseTime: '13:13',
+      total: '12.3',
+      items: [
+        { shortDescription: 'Pepsi - 12-oz', price: '1.25' }
+      ]
+    }
+
+    expect(contract.call(invalid_data)).to_not be_success
+  end
+
+  it 'rejects an invalid item price' do
+    invalid_data = {
+      retailer: 'Walgreens',
+      purchaseDate: '2022-01-02',
+      purchaseTime: '13:13',
+      total: '12.3',
+      items: [
+        { shortDescription: 'Pepsi - 12-oz', price: '1.25' },
+        { shortDescription: 'Dr. Salt - 24-oz', price: '25' }
+      ]
+    }
+
+    expect(contract.call(invalid_data)).to_not be_success
+  end
+end
